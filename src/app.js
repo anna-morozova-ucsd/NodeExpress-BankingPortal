04de3a083b637d0c4596e6c4da4e4c2bd4abdcda
anna-morozova-ucsd <amorozov@ucsd.edu>
2022-06-02 17:17:37 -0400

    Completed module

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+
+//3.configure views directory
+app.set(path.join(__dirname, '/views/'));
+app.set('view engine', 'ejs');
+//4. point express to public directory
+app.use(express.static(path.join(__dirname, '/public/')));
+
+//7.get route
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Index'});
+  });
+
+//8. create server  
+app.listen(3000,console.log('PS Project Running on port 3000!'))
+
